bf5f4ba7e24109bd3adda7d92acdbeeb2c8d7270
SSY1203 <tlstjsdud566@naver.com>
2023-05-05 17:00:58 +0900

    [feature] To Do List 및 Travel 하고 싶은 장소 입력할 수 있는 Input 추가

--- App.js
+++ App.js
@@ -1,27 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  TouchableHighlight,
-  TouchableWithoutFeedback,
-  Pressable,
-} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView } from 'react-native';
 import { theme } from './colors';
+import { useState } from 'react';
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState('');
+  const [toDos, setToDos] = useState({});
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+
+  const onChangeText = payload => setText(payload);
+
+  const addToDo = () => {
+    if (text === '') return;
+
+    // const newToDos = Object.assign({}, toDos, { [Date.now()]: { text, work: working } });
+    const newToDos = { ...toDos, [Date.now()]: { text, working } };
+    setToDos(newToDos);
+    setText('');
+  };
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity onPress={work}>
+          <Text style={{ ...styles.btnText, color: working ? 'white' : theme.grey }}>Work</Text>
         </TouchableOpacity>
-        <Pressable>
-          <Text style={styles.btnText}>Travel</Text>
-        </Pressable>
+        <TouchableOpacity onPress={travel}>
+          <Text style={{ ...styles.btnText, color: !working ? 'white' : theme.grey }}>Travel</Text>
+        </TouchableOpacity>
       </View>
+      <TextInput
+        onSubmitEditing={addToDo}
+        onChangeText={onChangeText}
+        value={text}
+        returnKeyType="done"
+        placeholder={working ? 'Add a To Do' : 'Where do U want to go?'}
+        style={styles.input}
+      />
     </View>
   );
 }
@@ -41,5 +59,25 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 40,
     fontWeight: '600',
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginVertical: 20,
+    fontSize: 18,
+  },
+  toDo: {
+    backgroundColor: theme.grey,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 40,
+    borderRadius: 15,
+  },
+  toDoText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '500',
   },
 });
